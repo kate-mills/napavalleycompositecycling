52a548be17b429d554d983ca66d483e2cffa3fe4
Kate Mills <helloworldloveksm@icloud.com>
2023-08-30 17:16:53 -0700

    Footer component

---
+++ src/components/Footer/imageData.js
@@ -0,0 +1,20 @@
+import s0 from './images/ibew.webp'
+import s1 from './images/redwood-trails-alliance.png'
+import s2 from './images/valero.webp'
+import s3 from './images/napa-bicycle-coalition.png'
+import s4 from './images/skyline.png'
+
+
+
+
+
+const supporters = [
+  { name: 'IBEW', url: s0, width: 80, height: 107 , href: 'https://www.ibewlu180.org/'},
+
+  { name: 'Redwood Trails Alliance', url: s1, width: 147, height: 63,href: 'https://trailsalliance.org/' },
+  { name: 'Valero Benicia Refinery', url: s2, width: 212, height: 45, href: 'https://www.valero.com/'},
+  { name: 'Napa County Bicycle Coalition', url: s3, width: 195, height: 56, href: 'https://napabike.org/' },
+  { name: 'Skyline Park', url: s4, width: 143, height: 63, href: 'https://www.skylinepark.org/' },
+]
+
+export default supporters
